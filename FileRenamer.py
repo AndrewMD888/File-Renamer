4d11379cf69f40bcbb0da1c75edf2b6bceff6515
Andrew D <45830729+AndrewMD888@users.noreply.github.com>
2019-05-21 06:25:04 -0400

    Update FileRenamer.py
    Removed all debugging print statements from the code.

--- FileRenamer.py
+++ FileRenamer.py
@@ -25,27 +25,17 @@ def my_rename(file_name):
         # If a match was not found (not alphanumeric), remove the non alphanumeric characters from the start of the string
         start_of_string_trimmed = re.sub(r'^[^A-Za-z0-9]+', r'', temp_string)
 
-        print(start_of_string_trimmed)
-
         # I chose 2 or more capital letters to match for below because this excludes first character capitalization, which is what I want here
         temp_string2 = re.sub(r'(^[A-Z]{2,})', r'\1_', start_of_string_trimmed)
-
-        print(temp_string2)
 
         # Inserts an underscore after the capturing group only if there are two or more consecutive lowercase letters in that group
         # Done so that the formatting of version numbers (for example, v1.03.54) is left as is
         temp_string3 = re.sub(r'((?<![A-Za-z\.])[a-z]{2,}(?=[a-z]*))', r'\1_', temp_string2)
 
-        print(temp_string3)
-
         temp_string4 = re.sub(r'((?<![A-Za-z])[0-9](?=[A-Za-z]))', r'\1_', temp_string3)
-
-        print(temp_string4)
 
         # Inserts an underscore after a sequence of 2 or more characters and before a number
         temp_string5 = re.sub(r'((?<![0-9])[A-Za-z]{2,}(?=[0-9]))', r'\1_', temp_string4)
-
-        print(temp_string5)
 
         # Gets the first character of the string for later usage
         save = re.search(r'(^[A-Z0-9]+)|(^[a-z0-9]+)', temp_string5).group()
@@ -64,8 +54,6 @@ def my_rename(file_name):
         # I chose 2 or more capital letters to match for below because this excludes first character capitalization, which I want excluded here
         temp_string2 = re.sub(r'(^[A-Z]{2,})', r'\1_', temp_string)
 
-        print(temp_string2)
-
         # Gets the first character of the string for later usage
         save = re.search(r'(^[A-Z0-9]+)|(^[a-z0-9]+)', temp_string2).group()
 
@@ -73,16 +61,10 @@ def my_rename(file_name):
         # Done so that the formatting of version numbers (for example, v1.03.54) is left as is
         temp_string3 = re.sub(r'((?<![A-Za-z\.])[a-z]{2,}(?=[a-z]*))', r'\1_', temp_string2)
 
-        print(temp_string3)
-
         temp_string4 = re.sub(r'((?<![A-Za-z])[0-9](?=[A-Za-z]))', r'\1_', temp_string3)
-
-        print(temp_string4)
 
         # Inserts an underscore after a sequence of 2 or more characters and before a number
         temp_string5 = re.sub(r'((?<![0-9])[A-Za-z]{2,}(?=[0-9]))', r'\1_', temp_string4)
-
-        print(temp_string5)
 
         # temp_string6 is now populated and is the same value as temp_string5
         temp_string6 = temp_string5
@@ -96,24 +78,16 @@ def my_rename(file_name):
         # Removes underscores where necessary
         temp_string6 = re.sub(r'((?<=[A-Za-z0-9_])[_]+(?![A-Za-z0-9]))', r'', temp_string5)
 
-        print(temp_string6)
-
         # Appends an underscore into the correct place for capturing group 2 after removing prior undesirable underscores
         temp_string7 = re.sub(r'((?<!^[a-z0-9])[_](?![A-Z0-9_]))|((?<!^[A-Z0-9])[_](?![a-z0-9][_]))', r'\2_', temp_string6)
-
-        print(temp_string7)
     
     elif temp_string6[1] == "_":
 
         # Removes underscores where necessary
         temp_string6 = re.sub(r'((?<=[A-Za-z0-9_])[_]+(?![A-Za-z0-9]))', r'', temp_string5)
 
-        print(temp_string6)
-
         # Appends an underscore into the correct place after removing prior undesirable underscores
         temp_string7 = re.sub(r'((?<!^[a-z0-9])[_](?![A-Z0-9_]))|((?<!^[A-Z0-9])[_](?![a-z0-9][_]))', r'\2_', temp_string6)
-
-        print(temp_string7)
 
     else:
 
@@ -122,24 +96,16 @@ def my_rename(file_name):
 
     # The first character of the string in lowercase now replaces whatever character is in the first position at this point
     temp_string8 = re.sub(r'(^[A-Z]+)|(^([A-Z]+)([a-z]+\.))', start_of_string_formatter, temp_string7)
-    
-    print(temp_string8)
 
     # Inserts underscores between lowercase letters and capital words properly
     # The "r'\1\2_'" syntax is the proper way to add an underscore after capturing group 1 and 2
     temp_string9 = re.sub(r'([a-z](?=[A-Z]))|([A-Z]{2,}(?=[a-z]))', r'\1\2_', temp_string8).lower()
 
-    print(temp_string9)
-
     # Removes any remaining extraneous/unnecessary underscores from the entire string
     temp_string10 = re.sub(r'((?<=[a-z])[_]+(?=[\.]))|((?<=[0-9])[_]+(?=[\.]))|((?<![a-z0-9])[_](?![A-Z]))', r'', temp_string9)
 
-    print(temp_string10)
-
     # Removes any amount of underscores on either side of a hyphen to avoid multiple consecutive separator characters
     final_string = re.sub(r'([_]{1,}(?=[-]))|((?<=[-])[_]{1,})', r'', temp_string10)
-
-    print(final_string)
 
     return final_string
 
